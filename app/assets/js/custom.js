645002139aeb3eb4ec65e0882918d93bc8235f68
Abhijit Mishra <abhijit.loyola11@gmail.com>
2019-11-14 20:35:21 +0530

    add modal, info card more added

--- app/assets/js/custom.js
+++ app/assets/js/custom.js
@@ -1,12 +1,39 @@
-if( $('.nav-item.active').hasClass( "user-menu" ) ) {
-    $('.username').removeClass('collapsed').attr("aria-expanded", true);
-}
-else {
-    $('.nav-item.active').parents('a[data-toggle]').removeClass('collapsed').attr("aria-expanded", true);
+var newField = `<div class="row">
+                    <div class="col-6">
+                        <div class="form-group">
+                        <label for="fieldName" class="bmd-label-floating">Field Name</label>
+                        <input type="text" class="form-control" id="fieldName">
+                        </div>   
+                    </div>
+
+                    <div class="col-6">
+                        <div class="form-group">
+                        <label for="fieldValue" class="bmd-label-floating">Field Value</label>
+                        <input type="text" class="form-control" id="fieldValue">
+                        </div>
+                    </div>
+                </div>
+                `;
+
+if ($(".nav-item.active").hasClass("user-menu")) {
+  $(".username")
+    .removeClass("collapsed")
+    .attr("aria-expanded", true);
+} else {
+  $(".nav-item.active")
+    .parents("a[data-toggle]")
+    .removeClass("collapsed")
+    .attr("aria-expanded", true);
 }
 
-$('.nav-item.active').closest('.collapse').addClass('show');
+$(".nav-item.active")
+  .closest(".collapse")
+  .addClass("show");
 
-$('.change-password-button').on('click', function(){
-    $("#change-password-fields").toggleClass('invisible');
-})
+$(".change-password-button").on("click", function() {
+  $("#change-password-fields").toggleClass("invisible");
+});
+
+$(".add-field-button").on("click", function() {
+  $(".modaL-form").append(newField);
+});
